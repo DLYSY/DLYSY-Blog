4604840616de1a19c75e429b8a1ba1593b3feddd
DLYSY <dly20011214@outlook.com>
2023-10-28 11:32:34 +0800

    None

--- download_hugo.py
+++ download_hugo.py
@@ -7,4 +7,4 @@ for i in a["assets"]:
     if i["name"].endswith("linux-amd64.deb") and "extended" in i["name"]:
         package_url=i["browser_download_url"]
 
-os.system("wget -O ${{ runner.temp }}/hugo.deb")
+os.system("wget -O ${{ runner.temp }}/hugo.deb {}".format(a))
